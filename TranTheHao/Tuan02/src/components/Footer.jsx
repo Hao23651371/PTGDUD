d40bcbf05da99bad04a734fa83a76d22c5984a47
FirstNam <tranhao180105@gmail.com>
2026-01-20 23:26:51 +0700

    Delete TranTheHao/Tuan02

--- TranTheHao/Tuan02/src/components/Footer.jsx
+++
@@ -1,5 +0,0 @@
-function Footer() {
-  return <footer>© 2026 - React Fundamentals</footer>;
-}
-
-export default Footer;
--- TranTheHao/Tuan02/src/components/Header.jsx
+++
@@ -1,5 +0,0 @@
-function Header() {
-  return <h1>Trang giới thiệu sinh viên</h1>;
-}
-
-export default Header;
